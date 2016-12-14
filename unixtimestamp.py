01b9b09636c05e905f9c10d41ae9e6232c7e717f
Craig Anderson <craiga@craiga.id.au>
2016-12-14 16:41:53 +0000

    Re-add content. Maybe the comment with a dot is the issue?

--- unixtimestamp.py
+++ unixtimestamp.py
@@ -1,3 +1,23 @@
 """
-unixtimesta.mp Flask application.
+Unix Timestamp Flask application.
 """
+
+import os
+from datetime import datetime
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/<int:timestamp>')
+def show_timestamp(timestamp):
+    """Display the current timestamp."""
+    return render_template('timestamp.html',
+                           timestamp=timestamp,
+                           datetime=datetime.fromtimestamp(timestamp))
+
+
+if __name__ == '__main__':
+    app.debug = bool(os.environ.get("DEBUG", True))
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
