4838f82c9033a8d8cc2c2fb914569a972782547a
Craig Anderson <craiga@craiga.id.au>
2018-11-21 12:15:15 +0000

    Black requires Python 3.6, and is broken on Travis' 3.7

--- unixtimestamp/__init__.py
+++ unixtimestamp/__init__.py
@@ -30,5 +30,5 @@ sentry = Sentry(
     app, logging=True, level=logging.getLevelName(app.config.get("LOG_LEVEL"))
 )
 
-# pylint: disable=wrong-import-position
+# noqa: E402 pylint: disable=wrong-import-position
 from unixtimestamp import views, error_handlers
